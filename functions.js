5f9cc6de558e1a62aa5ceec6f51f8d21f44758aa
james neufeld <jamesdneufeld@gmail.com>
2025-03-07 20:59:06 -0800

    updates

--- functions.js
+++ functions.js
@@ -1,13 +1,7 @@
 gsap.registerPlugin(Draggable, InertiaPlugin);
 
-Draggable.create("#navigation-link", {
-  type: "xy",
-  bounds: document.getElementById("container"),
+Draggable.create(".navigation-link", {
+  type: "x,y",
+  bounds: ".container",
   inertia: true,
-  onClick: function () {
-    console.log("clicked");
-  },
-  onDragEnd: function () {
-    console.log("drag ended");
-  },
 });
